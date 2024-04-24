eea480bc8964428f444d5d57cf5174f5a533ea24
bruno-henr <brunohenriquex00@gmail.com>
2024-04-24 09:56:19 -0300

    refac:  tipando retorno do controller, bem como alterando comportamento da classe

--- src/useCases/doctor/create/CreateDoctorController.ts
+++ src/useCases/doctor/create/CreateDoctorController.ts
@@ -2,11 +2,12 @@ import { Request, Response } from "express";
 import { CreateDoctorUseCase } from "./CreateDoctorUseCase";
 import { mediaProxy } from "../../../proxies";
 import { DoctorDTO } from "../../../dto/DoctorDTO";
+import { ICreateDoctorDTO } from "./DTO";
 
 export class CreateDoctorController {
-    constructor(private createDoctorUseCase: CreateDoctorUseCase) {}
+    constructor(private createDoctorUseCase: CreateDoctorUseCase) { }
 
-    async handle(request: Request, response: Response) {
+    async handle(request: Request, response: Response): Promise<Response> {
         const profileImage = request.file;
         let profileImageUrl = null;
 
@@ -20,23 +21,22 @@ export class CreateDoctorController {
                 );
             }
 
-            // creating  the new User
-            const doctorDto = new DoctorDTO(
-                "d3b8bbbf-d91f-4c1e-a639-6f2715d60899",
-                "John Doe",
-                "jony",
-                "1234567891012",
-                "jhon@email.com",
-                "hospial albert einstein",
-                profileImageUrl,
-            );
+            let doctorDto = {
+                id: "d3b8bbbf-d91f-4c1e-a639-6f2715d60899",
+                nome: "John Doe",
+                apelido: "jony",
+                crm: "1234567891012",
+                email: "jhon@email.com",
+                hospital: "hospial albert einstein",
+                imagem: profileImageUrl
+            }
 
             // executing the useCase
-            await this.createDoctorUseCase.execute(doctorDto);
-
-            return response.status(200).json("Created");
-        } catch (e) {
-            return response.status(400).json("Something went wrong: ");
+            const result = await this.createDoctorUseCase.execute(doctorDto);
+            if (result.has_error) return response.status(400).json(result);
+            return response.status(201).json(result);
+        } catch (e: any) {
+            return response.status(400).json({ error: e?.message });
         }
     }
 }
