c9bd136d3f526d08ccc52b07ee9ad92a2ae3afc5
bruno-henr <brunohenriquex00@gmail.com>
2024-04-24 22:56:39 -0300

    refac: adicionando generics na classe de response

--- src/utils/implementations/ResponseEntity.ts
+++ src/utils/implementations/ResponseEntity.ts
@@ -1,9 +1,9 @@
-export class ResponseEntity {
+export class ResponseEntity<T extends Object = any> {
     public ok: boolean;
     public message: string;
     public data: Object;
 
-    constructor(ok: boolean, message: string, data: Object) {
+    constructor(ok: boolean, message: string, data: T) {
         this.ok = ok;
         this.message = message;
         this.data = data;
