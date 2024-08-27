a4b280c9bdd2dff52987be8f99d23d2f5155cedb
adriellyEster <adriellyester9@gmail.com>
2024-08-26 23:10:17 -0300

    rotas para arquivos

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,29 +1,42 @@
 import { Router, Express } from "express";
-import multer from "multer";
-import path from "path";
-import { FirebaseMediaProxy } from "../proxies/imp/FirebaseMediaProxy";
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+import { body, validationResult } from "express-validator";
+import { createFileController } from "../useCases/files/create";
+import { listFileController } from "../useCases/files/list";
+import { putFileController } from "../useCases/files/put";
+import { deleteFileController } from "../useCases/files/delete";
 
 const uploadRouter = Router();
 
-uploadRouter.post('/upload', upload.array('image', 10), async (req, res) => {
-    const firebase = new FirebaseMediaProxy();
-    try {
-        console.log('files => ', req.files[0])
-        let uploadedFiles = []
-        
-        for (const file of req.files as Express.Multer.File[]) {
-            const fileType = file.mimetype.split("/")[1];
-            const filePath = `${req.body.id}/${Date.now()}.${fileType}`;
-            const url = await firebase.saveImage(file.buffer, filePath);
-            uploadedFiles.push(url)
+uploadRouter.post("/",
+    body(["url", "postagemId"])
+        .notEmpty()
+        .escape()
+        .withMessage("Field Cannot Be Empty"),
+    (req, res) => {
+        const result = validationResult(req.body);
+
+        if (result.isEmpty()) {
+            return createFileController.handle(req, res);
         }
-        res.send(uploadedFiles);
-    } catch (err) {
-        res.status(400).send(err);
+
+        return res.status(400).json({
+            ok: false,
+            message: `All fields must be not fullfield`,
+            erros: result.array(),
+        });
     }
-});
+);
+
+uploadRouter.get("/",
+    (req, res) => listFileController.handle(req, res)
+);
+
+uploadRouter.put("/",
+    (req, res) => putFileController.handle(req, res)
+);
+
+uploadRouter.delete("/:id",
+    (req, res) => deleteFileController.handle(req, res)
+);
 
 export { uploadRouter };
